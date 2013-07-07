8dd01bfde83592ee7469f70ffbd8b0e61a186e62
Anando Purutama <anando.purutama@gmail.com>
2013-07-06 19:54:13 -0500

    holas5

--- web.js
+++ web.js
@@ -1,12 +1,21 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
+var express = require('express'),
+app = express.createServer(express.logger()),
+http = require('http'),
+fs = require('fs');
 
 app.get('/', function(request, response) {
-  response.send('hola mundo que tal22!');
+response.writeHead(200, {'Content-Type': 'text/html'});
+response.write(fs.readFileSync("./index.html", "utf8").toString());
+response.end();
 });
 
-var port = process.env.PORT || 5000;
+app.get('/main.css', function(request, response) {
+response.writeHead(200, {'Content-Type': 'text/css'});
+response.write(fs.readFileSync("./main.css", 'utf8').toString());
+response.end();
+});
+
+var port = process.env.PORT || 8000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
